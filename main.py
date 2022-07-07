c5df70585e6518201eceff3539d24972b1e85094
SanAntonik <sanantonik@gmail.com>
2022-07-07 12:55:12 +0300

    Try Jupyter Notebooks to explore your data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from load_data import load_data
+
+
 def main():
-    pass
+    load_data(PATH)
 
 
 if __name__ == '__main__':
+    PATH = "C:/Users/San/Documents/CS projects/yt_activity_analysis/data/Takeout/YouTube and YouTube Music/history/watch-history.json"
     main()
