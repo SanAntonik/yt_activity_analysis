04b2a8208a04494a965cabfafe58f47a2af4995c
SanAntonik <sanantonik@gmail.com>
2022-07-20 17:49:03 +0300

    Break plotting function into two in plotting.py and plot first n most watched channels on the same figure

--- plotting.py
+++ plotting.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def plot_videos_per_h(df, channel_to_plot):
-    df_for_channel = df.loc[df["channel_name"] == channel_to_plot]
+def get_videos_per_h_series(df, channel_name):
+    df_for_channel = df.loc[df["channel_name"] == channel_name]
     # get hours from datetime
     get_hs = pd.DatetimeIndex(df_for_channel['time']).hour
     # count on how many videos you clicked and sort hours from 0 to 23
@@ -12,8 +12,12 @@ def plot_videos_per_h(df, channel_to_plot):
     series = pd.Series(count_videos_per_h, hs_of_the_day)
     # change NaN to 0
     series = series.fillna(0)
-    # change dtype to int
-    series = series.astype(int)
+    # change dtype to int and return
+    return series.astype(int)   
+
+
+def one_channel_bar_plot_videos_per_h(series, channel_name):
+    series = get_videos_per_h_series(series, channel_name)
     series.plot.bar(color="purple", rot=0,
-                    title=f"{channel_to_plot}",
+                    title=f"{channel_name}",
                     xlabel="hour", ylabel="videos per hour")
