dbd917c7473fa19c1574b4f3adb5376412d85574
SanAntonik <sanantonik@gmail.com>
2022-08-22 19:11:28 +0300

    Modify plotting functions to handle all df and put all imports into one cell

--- plotting.py
+++ plotting.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 
-def get_videos_per_h_series(df, channel_name):
-    df_for_channel = df.loc[df["channel_name"] == channel_name]
-    # get hours from datetime
-    get_hs = pd.DatetimeIndex(df_for_channel['time']).hour
+def get_videos_per_h_series(df, channel_name=""):
+    if not channel_name:
+        get_hs = pd.DatetimeIndex(df['time']).hour
+    else:
+        df_for_channel = df.loc[df["channel_name"] == channel_name]
+        # get hours from datetime
+        get_hs = pd.DatetimeIndex(df_for_channel['time']).hour
     # count on how many videos you clicked and sort hours from 0 to 23
     count_videos_per_h = get_hs.value_counts().sort_index()
     hs_of_the_day = [h for h in range(0, 24)]
@@ -16,7 +19,7 @@ def get_videos_per_h_series(df, channel_name):
     return series.astype(int)   
 
 
-def one_channel_bar_plot_videos_per_h(series, channel_name):
+def bar_plot_videos_per_h(series, channel_name=""):
     series = get_videos_per_h_series(series, channel_name)
     series.plot.bar(color="purple", rot=0,
                     title=f"{channel_name}",
