57dda96fc2eddf8893c3dde5543dfba9d9a4beaa
SanAntonik <sanantonik@gmail.com>
2022-09-13 20:31:37 +0300

    See how you used a particular channel over months

--- plotting.py
+++ plotting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def get_videos_per_h_series(df, channel_name=""):
+def get_videos_per_h(df, channel_name):
     if not channel_name:
         get_hs = pd.DatetimeIndex(df['time']).hour
     else:
@@ -10,8 +10,8 @@ def get_videos_per_h_series(df, channel_name=""):
         get_hs = pd.DatetimeIndex(df_for_channel['time']).hour
     # count on how many videos you clicked and sort hours from 0 to 23
     count_videos_per_h = get_hs.value_counts().sort_index()
-    hs_of_the_day = [h for h in range(0, 24)]
     # get full range of x values from 0 to 23
+    hs_of_the_day = [h for h in range(0, 24)]
     series = pd.Series(count_videos_per_h, hs_of_the_day)
     # change NaN to 0
     series = series.fillna(0)
@@ -19,8 +19,29 @@ def get_videos_per_h_series(df, channel_name=""):
     return series.astype(int)
 
 
-def bar_plot_videos_per_h(series, channel_name=""):
-    series = get_videos_per_h_series(series, channel_name)
+def h_bar_plot(df, channel_name=""):
+    series = get_videos_per_h(df, channel_name)
     series.plot.bar(color="purple", rot=0,
                     title=f"{channel_name}",
                     xlabel="hour", ylabel="videos per hour")
+
+
+def month_bar_plot(df, channel_name=""):
+    series = get_videos_per_month(df, channel_name)
+    series.plot.bar(color="purple", rot=0,
+                    title=f"{channel_name}",
+                    xlabel="hour", ylabel="videos per month")
+
+
+def get_videos_per_month(df, channel_name):
+    if not channel_name:
+        # Per every row get month of recording. Then count values
+        videos_per_month = df['time'].dt.strftime('%b').value_counts()
+    else:
+        df_for_channel = df.loc[df["channel_name"] == channel_name]
+        videos_per_month = df_for_channel['time'].dt.strftime('%b').value_counts()
+    str_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    # Add empty months
+    videos_per_month = videos_per_month.reindex(str_months, axis=0)
+    # Change NaN in those empty months to 0. Then change dtype to int
+    return videos_per_month.fillna(0).astype(int)
