4691198a1a8fdf312f2aa41d4de141697b743d70
SanAntonik <sanantonik@gmail.com>
2022-09-20 16:26:48 +0300

    Don't drop 'header' col in preparation

--- preparation.py
+++ preparation.py
@@ -17,7 +17,7 @@ def prepare_data(PATH):
     # Change timezone from UTC to mine
     df["time"] = df["time"].dt.tz_convert('Europe/Kiev')
     # Drop unimportant columns
-    df.drop(["header", "titleUrl", "products", "activityControls",
+    df.drop(["titleUrl", "products", "activityControls",
             "description", "details"], axis=1, inplace=True)
     # Remove "Watched " from every row of column "title"
     df["title"] = [i[8:] for i in df['title']]
@@ -33,11 +33,11 @@ def prepare_data(PATH):
     # Change dtypes of cols 'title' and 'channel_name' from object to string
     colnames = ["title", "channel_name"]
     df[colnames] = df[colnames].astype("string")
-    # Rename col 'title' to 'video_title'
-    df.rename(columns={'title': 'video_title'}, inplace=True)
+    # Rename col 'title' to 'video_title' and col 'header' to 'app'
+    df.rename(columns={"title": "video_title", "header": "app"}, inplace=True)
     # Rearrange order of columns
     cols = df.columns.tolist()
-    cols = [cols[0]] + [cols[2]] + [cols[1]]
+    cols = [cols[1]] + [cols[3]] + [cols[2]] + [cols[0]]
     df = df[cols]
     return df
 
