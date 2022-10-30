e14c11a564a11e35ebdc71b763dd7128bc645d9e
SanAntonik <sanantonik@gmail.com>
2022-10-30 14:09:49 +0200

    Add comment that explains how to handle right side of date interval

--- preparation.py
+++ preparation.py
@@ -48,11 +48,23 @@ def select_by_date_interval(df, start, end=""):
     """
     Summary:
         Select a subset of a df based on the specified
-        time interval.
-        Important: input points of the interval
+        time interval
+
+    Important 1:
+        Input points of the interval
         must be in the year-month-day format.
         For example, 2022-07-15 that corresponds to
         July 15, 2022
+
+    Important 2:
+        Right part of the interval is not included.
+        For example, if you choose interval from 2022-07-15
+        to 2022-07-28, you won't get the last day (2022-07-28).
+        Therefore, take one day more on the right interval edge.
+        If you want to include an entire month, set your right date
+        to the start of the next month. For example, to include
+        entire September 2022,  set left date to 2022-09-01 and
+        right date to 2022-10-01
 
     Args:
         df (df): DataFrame
@@ -68,5 +80,5 @@ def select_by_date_interval(df, start, end=""):
     # if you didn't pass the ending date, use the most recent one
     if not end:
         end = df["time"].iloc[0]
-    mask = (df['time'] > start) & (df['time'] <= end)
+    mask = (df['time'] >= start) & (df['time'] < end)
     return df.loc[mask]
